090d9cc5f03d14c494039ecec5c977fde2fca85d
MariyaBenson <mariyafrancis@loyalistcollege.com>
2025-02-28 00:08:34 -0500

    chart remove

--- src/components/ChartComponent.js
+++ src/components/ChartComponent.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Bar } from "react-chartjs-2";
-import Chart from "chart.js/auto";
 
 const ChartComponent = ({ actualPrices, predictedPrices, labels }) => {
   const [chartData, setChartData] = useState({
